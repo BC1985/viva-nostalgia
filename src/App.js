db439a66c7f7451c00a53f0ace4f96785a959416
Ben Cohen <benjcohen1985@gmail.com>
2021-02-10 11:47:55 +0200

    Import components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import LandingPage from "./components/LandingPage/LandingPage"
+import CoffeeMenu from "./components/CoffeeMenu/CoffeeMenu";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import './App.css';
+import Shop from './components/Shop/Shop';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+        <Route path="/shop" component={Shop}/>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/menu" component={CoffeeMenu}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
