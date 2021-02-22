ccaa134b9cea3246f218899cd0c0ec7c411109ca
Ben Cohen <benjcohen1985@gmail.com>
2021-02-22 15:43:53 +0200

    Add quantity dropdown

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./cart.css";
+import { Context } from "../../Context";
+
 function CartItem({ item }) {
+  const { increaseQty } = useContext(Context);
+
+  item.qty = 1;
+  const options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const dropDown = options.map(x => {
+    return <option key={x}>{x}</option>;
+  });
+
   return (
     <div className="cart-item-container">
       <img src={item.url} className="cart-img" />
       <span>
         {item.itemName}-{item.price}$
       </span>
+      <select onChange={e => increaseQty(item, e.target.value)}>
+        {dropDown}
+      </select>
     </div>
   );
 }
